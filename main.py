6250182bc631adce0fe875fee9e9b915a4857fb7
TheAwsomeCoder926 <108197957+TheAwsomeCoder926@users.noreply.github.com>
2023-08-26 18:33:47 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# Imports
+import turtle
+import player
+import car
+
+# Objects
+cars = car.Car()
+player1 = player.Player()
+screen = turtle.Screen()
+
+# Setup
+screen.setup(width=800, height=450, startx=100, starty=None)
+
+player1.seth(90)  # Set the heading to go up
+player1.goto(0, -200)  # Make it go to the bottom of the Window
+
+turtle.listen()
+turtle.onkey(player1.player_forward, "space")  # Makes player go forward
+turtle.onkey(player1.player_backward, "Right")  # Makes the player go back ("It's a Dev thing")
+
+running = True  # The while loop thing
+levels_beat = 0  # The amount of levels beat
+
+while running:
+    # Checking if player beat the level
+    if player1.ycor() >= 215:
+        levels_beat += 1
+        print(levels_beat)  # FIND ~ remove pls
+        player1.goto(0, -200)
